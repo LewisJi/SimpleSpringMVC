450b69531914bbb81376276e2af75e1dc014488e
LewisJi <zou0711@gmail.com>
2024-04-15 14:37:00 +0800

    test try update test update update update update

---
+++ src/main/java/com/lewis/repo/HelloRepository.java
@@ -0,0 +1,11 @@
+package com.lewis.repo;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HelloRepository {
+	
+	public String qryHello() {
+		return "Hello, Spring MVC   !";
+	}
+}
